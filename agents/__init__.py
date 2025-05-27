59ff436ab046294d2c9f2775b304102b3166824f
Ahad Raza <ahadraza30@gmail.com>
2025-05-27 20:31:58 +0500

    Changed the class name

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,13 +1,13 @@
 from .base_agent import Agent
 from .news_retrieval_agent import NewsRetrieverAgent
 from .fact_check_agent import FactCheckAgent
-from .bias_analysis_agent import BiasAnalysisAgent
+from .bias_analysis_agent import BiasDetectionAgent
 from .llm_agent import LLMDecisionAgent
 
 __all__ = [
     'Agent',
     'NewsRetrieverAgent',
     'FactCheckAgent',
-    'BiasAnalysisAgent',
+    'BiasDetectionAgent',
     'LLMDecisionAgent'
 ] 
